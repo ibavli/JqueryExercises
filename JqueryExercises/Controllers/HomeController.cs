4ba7634ef7d4db8b15cc023e809f6f7e2463c920
ibavli <ibrahim.bavli35@gmail.com>
2019-02-08 10:04:10 +0300

    first commit

---
+++ JqueryExercises/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryExercises.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult JqueryDegiskenTanimlama()
+        {
+            return View();
+        }
+
+
+        public ActionResult JqueryDegiskenIcerisineFonksiyonTanimlama()
+        {
+            return View();
+        }
+
+
+        public ActionResult ButonaClickTanimlama()
+        {
+            return View();
+        }
+
+
+        public ActionResult DiziTanimlamaVeDegerleriniAlma()
+        {
+            return View();
+        }
+
+
+        public ActionResult TagIleHtmlElemanıSecme()
+        {
+            return View();
+        }
+
+
+        public ActionResult CssIleHtmlElemanıSecme()
+        {
+            return View();
+        }
+
+
+        public ActionResult NameIleHtmlElemanıSecme()
+        {
+            return View();
+        }
+
+
+        public ActionResult IndexeGoreElemanıSecme()
+        {
+            return View();
+        }
+
+
+        public ActionResult OzellikVeTurIleElemaniSecme()
+        {
+            return View();
+        }
+
+
+        public ActionResult EtiketinAltEtiketleriniSecme()
+        {
+            return View();
+        }
+
+
+        public ActionResult BirdenFazlaDivSecme()
+        {
+            return View();
+        }
+
+
+        public ActionResult IsKullanimi()
+        {
+            return View();
+        }
+    }
+}
