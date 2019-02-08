f5d1ce839afcf50ce5b0c2911c6537980c6d5b6f
ibavli <ibrahim.bavli35@gmail.com>
2019-02-08 19:59:17 +0300

    multifileupload and numberofcharacters

---
+++ JqueryChooseExercises/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using JqueryChooseExercises.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryChooseExercises.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult TumKayitlariSec()
+        {
+            List<Urun> urunler = new List<Urun>()
+            {
+                new Urun
+                        {
+                            Id = 1, Ad = "Bilgisayar", Fiyat = 1200
+                        },
+                        new Urun
+                        {
+                            Id = 2, Ad = "Notebook", Fiyat = 2200
+                        },
+                        new Urun
+                        {
+                            Id = 3, Ad = "Tablet PC", Fiyat = 800
+                        },
+                        new Urun
+                        {
+                            Id = 4, Ad = "Netbook", Fiyat = 600
+                        },
+                        new Urun
+                        {
+                            Id = 5, Ad = "Akıllı Telefon", Fiyat = 1750
+                        }
+            };
+            ViewBag.urunler = urunler;
+            return View();
+
+        }
+
+        [HttpPost]
+        public void SecilileriSil(string seciliIdler)
+        {
+            
+        }
+
+
+        
+    }
+}
---
+++ JqueryMultiFileUpload/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryMultiFileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult DosyaYukleme()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DosyaYukleme(IEnumerable<HttpPostedFileBase> dosyalar)
+        {
+            foreach (var dosya in dosyalar)
+            {
+                if (dosya != null)
+                {
+                    string dosyaYolu = Path.GetFileName(dosya.FileName);//Dosya yolunu al.
+
+                    var yuklenecekYer = Path.Combine(Server.MapPath("~/Dosyalar"), dosyaYolu);//Dosyanın yüklenecek yolunu al.
+
+                    dosya.SaveAs(yuklenecekYer);//Dosyayı yüklenecek yere yükle.
+                }
+            }
+        
+            return View();
+        }
+
+    }
+}
